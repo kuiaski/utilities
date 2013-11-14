cebc582d71cba95c68a80e86ea5749ce42e89326
José Rosa Kuiaski <kuiaski@denke.cc>
2013-11-14 13:49:33 -0200

    Testing Commit from Visual Studio 2013

---
+++ Utilities.Queues/FixedSizeQueue.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * I saw this solution on StackOverflow. It helped me a lot of times.
+ */
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Queues
+{
+    public class FixedSizeQueue<T> : ConcurrentQueue<T>
+    {
+        /// <summary>
+        /// Size of the queue.
+        /// </summary>
+        public int Size { get; private set; }
+
+        public FixedSizeQueue(int size)
+        {
+            Size = size;
+        }
+
+        /// <summary>
+        /// Adds a new item in the Queue. Old values will be dequeued.
+        /// </summary>
+        /// <param name="item">Item do be queued.</param>
+        public new void Enqueue(T item)
+        {
+            base.Enqueue(item);
+            lock(this)
+            {
+                while (base.Count > Size)
+                {
+                    T overflow;
+                    base.TryDequeue(out overflow);
+                }
+            }
+        }
+    }
+}
---
+++ Utilities.Queues/IntervalQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Queues
+{
+    public class IntervalQueue<T> : ConcurrentQueue<T>
+    {
+        /// <summary>
+        /// Window Size.
+        /// </summary>
+        public TimeSpan Interval { get; private set; }
+
+        /// <summary>
+        /// Name of the Time Property.
+        /// </summary>
+        public string TimePropertyName { get; private set; }
+
+        public IntervalQueue(TimeSpan interval)
+        {
+            Interval = interval;
+        }
+
+        public IntervalQueue(TimeSpan interval, string timePropertyName)
+        {
+            Interval = interval;
+            TimePropertyName = timePropertyName;
+
+        }
+
+        // TODO: Implementing method to ensure Type has Time Property.
+        private void EnsureTypeHasProperty()
+        {
+            if (string.IsNullOrEmpty(TimePropertyName))
+            {
+                //if (TypeHasTimeProperty("Timestamp")) 
+            }
+            else
+            {
+
+            }
+            //throw new MissingFieldException(type.Name, TimePropertyName);
+        }
+
+        /// <summary>
+        /// Checks if type T has a property called 'TimePropertyName' of type 'DateTime'.
+        /// </summary>
+        /// <param name="timePropertyName">Property Name</param>
+        /// <returns>If property exists and is a DateTime.</returns>
+        private bool TypeHasTimeProperty(string timePropertyName)
+        {
+            Type type = typeof(T);
+            PropertyInfo property = type.GetProperty(timePropertyName);
+            if (property == null)
+            {
+                return false;
+            }
+
+            return property.PropertyType == typeof(DateTime);
+        }
+        public new void Enqueue(T item)
+        {
+
+        }
+    }
+}
