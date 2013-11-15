cda85a69184ecda068dc37b5451de20efa3b8d7d
José Rosa Kuiaski <kuiaski@denke.com.br>
2013-11-15 19:34:29 -0200

    Added Dequeue Unit Test for IntervalQueue

--- Utilities.Tests/QueuesTests.cs
+++ Utilities.Tests/QueuesTests.cs
@@ -35,6 +35,33 @@ namespace Utilities.Tests
         }
 
         /// <summary>
+        /// Checks if the FixedSizeQueue correctly dequeues.
+        /// </summary>
+        [Fact]
+        public void FixedSizeQueueCorrectlyDequeues()
+        {
+            int expected = 3;
+
+            FixedSizeQueue<int> queue = new FixedSizeQueue<int>(10);
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            int overflow;
+            queue.TryDequeue(out overflow);
+            queue.TryDequeue(out overflow);
+
+            int actual;
+            queue.TryDequeue(out actual);
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
         /// Tests if the IntervalQueue will respect Time Window.
         /// </summary>
         [Fact]
@@ -68,6 +95,47 @@ namespace Utilities.Tests
             List<QueueItem> actual = queue.ToList();
 
             Assert.Equal<QueueItem>(expected, actual);
+        }
+
+        /// <summary>
+        /// Checks if the IntervalQueue Correctly Dequeues.
+        /// </summary>
+        [Fact]
+        public void IntervalQueueCorrectlyDequeues()
+        {
+            TimeSpan windowSize = TimeSpan.FromDays(5);
+            IntervalQueue<QueueItem> queue = new IntervalQueue<QueueItem>(windowSize, "EventTime");
+
+            DateTime initialDate = DateTime.UtcNow;
+            int initialValue = 0;       
+
+            queue.Enqueue(new QueueItem()
+            {
+                EventTime  = initialDate.AddDays(1),
+                Value = initialValue++,
+            });
+
+            QueueItem expected = new QueueItem()
+            {
+                EventTime = initialDate.AddDays(2),
+                Value = initialValue++,
+            };
+
+            queue.Enqueue(expected);
+
+            queue.Enqueue(new QueueItem()
+            {
+                EventTime = initialDate.AddDays(3),
+                Value = initialValue++,
+            });
+
+            QueueItem overflow;
+            queue.TryDequeue(out overflow);
+
+            QueueItem actual;
+            queue.TryDequeue(out actual);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
